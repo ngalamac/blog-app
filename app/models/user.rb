f873919d16673b9f951a6ee592ab4f0ae73cb1fb
NGALA MAC <macngala4@gmail.com>
2023-11-22 06:31:10 +0100

    Add devise implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: :author_id, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
-  
-    def recent_posts(limit = 3)
-      posts.order(created_at: :desc).limit(limit)
-    end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
   end
+end
